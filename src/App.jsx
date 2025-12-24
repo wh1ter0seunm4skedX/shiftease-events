4098237979c8cb58429b495470c30f7e2b047b0b
Michael <wh1ter0seunm4sked@gmail.com>
2025-12-24 20:02:08 +0200

    Simplify app to placeholder landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 // Toaster is mounted globally in index.jsx
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import { LanguageProvider, useLanguage } from './contexts/LanguageContext';
-import SignIn from './components/auth/SignIn';
-import SignUp from './components/auth/SignUp';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import EventDashboard from './components/events/EventDashboard';
-import EventsArchive from './components/admin/EventsArchive';
 
 function LandingPage() {
   return (
@@ -47,50 +39,8 @@ function LandingPage() {
   );
 }
 
-function AppContent() {
-  useEffect(() => {
-    document.documentElement.dir = 'rtl';
-    document.body.dir = 'rtl';
-    document.body.classList.add('rtl');
-  }, []);
-
-  return (
-    <div className="min-h-screen bg-gray-50 rtl">
-      <Routes>
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <EventDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/archive"
-          element={
-            <ProtectedRoute>
-              <EventsArchive />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
-    </div>
-  );
-}
-
 function App() {
-  return (
-    <Router>
-      <AuthProvider>
-        <LanguageProvider>
-          <AppContent />
-        </LanguageProvider>
-      </AuthProvider>
-    </Router>
-  );
+  return <LandingPage />;
 }
 
 export default App;
